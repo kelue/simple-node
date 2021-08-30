b5747b03f94e6d680b9eb47e25f298dff67b288e
kelue <austincmoon5@gmail.com>
2021-08-30 02:22:29 +0100

    added env variables in server.js file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log("Microservices rock!");
+    const anime = process.env.FAVORITE_AINIME;
+    console.log(`My favorite anime is ${anime}`);
     await sleep(5000);
   }
 }
